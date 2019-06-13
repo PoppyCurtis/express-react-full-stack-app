b5677de32a933da3c82f52731fdaba5c4939396d
PoppyCurtis <poppy.curtis@gmail.com>
2019-06-13 21:19:31 +0100

    Add components to the dashboard and use connect feature in Redux

---
+++ src/app/components/Main.jsx
@@ -0,0 +1,13 @@
+import { store } from '../store';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedDashboard } from './Dashboard';
+
+export const Main = () => (
+    <Provider store={store}>
+        <div>
+            {/* Dashboard goes here */}
+           <ConnectedDashboard/>
+        </div>
+    </Provider>
+)
